b23d8c4872d1b37a0b33fec1403844aea881b094
Chase Lee <chaseklee990@gmail.com>
2018-03-09 18:03:28 -0800

    (feat) Implement functions to setclient ID and check error

--- twitch/api.go
+++ twitch/api.go
@@ -1,11 +1,22 @@
 package twitch
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-func init() {
-	fmt.Println("Initializing Twitch API...")
+var tID string
+
+const accept string = "application/vnd.twitchtv.v5+json"
+
+// SetID => takes clientID and sets variable for use in header
+func SetID(clientID string) {
+	fmt.Println("Setting clientID")
+	tID = clientID
 }
 
-func DoSomething() {
-	fmt.Println("Something.")
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
