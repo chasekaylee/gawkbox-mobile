37d6bdd4e7be849e86b90f5b934d63297e7ad207
Chase Lee <chaseklee990@gmail.com>
2018-03-09 18:15:09 -0800

    (feat) Implement methods for GetStreamer and GetUserStream

--- twitch/api.go
+++ twitch/api.go
@@ -1,8 +1,12 @@
 package twitch
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
 )
 
 var tID string
@@ -19,4 +23,67 @@ func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// GetStreamer => takes in a search query username
+// returns a Streamer struct for that specified username
+func GetStreamer(name string) Streamer {
+	var streamer Streamers
+
+	twitClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, "https://api.twitch.tv/kraken/users?login="+name, nil)
+	checkErr(err)
+
+	req.Header.Set("Accept", accept)
+	req.Header.Add("Client-ID", tID)
+
+	res, getErr := twitClient.Do(req)
+	checkErr(getErr)
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	checkErr(readErr)
+	res.Body.Close()
+
+	err = json.Unmarshal([]byte(body), &streamer)
+	checkErr(err)
+
+	t := streamer.Users[0]
+
+	return t
+}
+
+// GetUserStream => takes in a channel ID
+// returns a Stream struct for that specified channel ID
+func GetUserStream(id string) (StreamData, bool) {
+	var stream StreamData
+	var online = true
+
+	twitClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, "https://api.twitch.tv/kraken/streams/"+id, nil)
+	checkErr(err)
+
+	req.Header.Set("Accept", accept)
+	req.Header.Add("Client-ID", tID)
+
+	res, getErr := twitClient.Do(req)
+	checkErr(getErr)
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	checkErr(readErr)
+	res.Body.Close()
+	fmt.Println(string(body))
+	if string(body) == `{"stream":null}` {
+		online = false
+	}
+
+	err = json.Unmarshal([]byte(body), &stream)
+	checkErr(err)
+
+	return stream, online
 }
