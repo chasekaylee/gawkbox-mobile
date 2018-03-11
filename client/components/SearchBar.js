775504bef5a4e93e88393834346377a64b6db631
Chase Lee <chaseklee990@gmail.com>
2018-03-11 15:02:02 -0700

    (feat) Implement search bar

---
+++ client/components/SearchBar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, WebView } from 'react-native';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+    };
+  }
+
+  render() {
+    const { container, search, button } = styles;
+    return (
+      <View style={container}>
+        <TextInput
+          style={search}
+          onChangeText={query => this.setState({ query })}
+          value={this.state.query}
+          placeholder="Search.."
+        />
+        <Button
+          style={button}
+          title={this.props.loading ? 'loading...' : 'Search'}
+          onPress={() => this.props.search(this.state.query)}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  search: {
+    flex: 1,
+  },
+  button: {
+    height: 30,
+    marginBottom: 8,
+  },
+});
